4f32decdcbba71580a56503d67d3634d31f93377
oxidetor <pgirdhar@my.bcit.ca>
2020-11-17 20:30:03 -0800

    Added modify/delete dialog to items in RecyclerView on Long-click

--- app/src/main/java/com/bcit/teng_prateek/RecyclerAdapter.java
+++ app/src/main/java/com/bcit/teng_prateek/RecyclerAdapter.java
@@ -1,12 +1,14 @@
 package com.bcit.teng_prateek;
 
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -19,27 +21,73 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         this.readingList = readingList;
     }
 
-
-    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView date;
         TextView sysdias;
         TextView note;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull final View itemView) {
             super(itemView);
 
             date = itemView.findViewById(R.id.readingRowDateTextView);
             sysdias = itemView.findViewById(R.id.readingRowSysDiasTextView);
             note = itemView.findViewById(R.id.readingRowNoteTextView);
 
-            itemView.setOnClickListener(this);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Toast.makeText(itemView.getContext(), "SHORT" + getAdapterPosition(),Toast.LENGTH_SHORT).show();
 
-        }
+                }
+            });
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+//                    Toast.makeText(itemView.getContext(), "LONG" + getAdapterPosition(),Toast.LENGTH_SHORT).show();
 
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(itemView.getContext(), readingList.get(getAdapterPosition()).getId(), Toast.LENGTH_SHORT).show();
+                    LayoutInflater inflater = this.getLayoutInflater();
+                    View view = inflater.inflate(R.layout.update_delete_dialog, null);  // this line
+
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(v.getContext());
+                    alertDialog.setView(view);
+
+                    EditText systolicUD = view.findViewById(R.id.editTextSystolicUD);
+                    EditText diastolicUD = view.findViewById(R.id.editTextDiastolicUD);
+
+
+                    systolicUD.setText("" + readingList.get(getAdapterPosition()).getSystolic());
+                    diastolicUD.setText("" + readingList.get(getAdapterPosition()).getDiastolic());
+
+                    alertDialog.setTitle("Modify / Delete Reading");
+                    alertDialog.setPositiveButton("UPDATE",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                    alertDialog.setNeutralButton("DELETE",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                    alertDialog.setNegativeButton("CANCEL",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                    alertDialog.show();
+                    return false;
+                }
+
+                private LayoutInflater getLayoutInflater() {
+                    return LayoutInflater.from(itemView.getContext());
+                }
+            });
+
         }
     }
 
@@ -54,7 +102,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.date.setText(readingList.get(position).getDatetime());
         holder.sysdias.setText(readingList.get(position).getSystolic() + "/" + readingList.get(position).getDiastolic());
         holder.note.setText(getNote(readingList.get(position).getSystolic(), readingList.get(position).getDiastolic()));
